2ca70727105824979a8d10b6858cb61049a9303f
Nick <goldenquap@gmail.com>
2022-11-28 01:13:52 -0500

    Added server and client

---
+++ PA2_receiver.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# Last updated: Oct, 2021
+
+import sys
+import time
+import socket
+import datetime 
+from checksum import checksum, checksum_verifier
+
+CONNECTION_TIMEOUT = 60 # timeout when the receiver cannot find the sender within 60 seconds
+FIRST_NAME = "FIRSTNAME"
+LAST_NAME = "LASTNAME"
+
+def start_receiver(server_ip, server_port, connection_ID, loss_rate=0.0, corrupt_rate=0.0, max_delay=0.0):
+    """
+     This function runs the receiver, connnect to the server, and receiver file from the sender.
+     The function will print the checksum of the received file at the end. 
+     The checksum is expected to be the same as the checksum that the sender prints at the end.
+
+     Input: 
+        server_ip - IP of the server (String)
+        server_port - Port to connect on the server (int)
+        connection_ID - your sender and receiver should specify the same connection ID (String)
+        loss_rate - the probabilities that a message will be lost (float - default is 0, the value should be between 0 to 1)
+        corrupt_rate - the probabilities that a message will be corrupted (float - default is 0, the value should be between 0 to 1)
+        max_delay - maximum delay for your packet at the server (int - default is 0, the value should be between 0 to 5)
+
+     Output: 
+        checksum_val - the checksum value of the file sent (String that always has 5 digits)
+    """
+
+    print("Student name: {} {}".format("Nicholas", "Ankiewicz"))
+    print("Start running receiver: {}".format(datetime.datetime.now()))
+
+    checksum_val = "00000"
+    def make_ack(ack_num):
+        if (ack_num != 0 and ack_num != 1):
+            print("Invalid ack_num")
+            return None
+        ack = "  {}                      ".format(ack_num)
+        
+        if len(ack) != 25:
+            print("Ack lenght error")
+            return None
+        ack = ack + checksum(ack)
+        if len(ack) != 30:
+            print("checksum length error")
+            return None
+        if not checksum_verifier(ack):
+            print("Checksum validation incorrect")
+            return None
+        return ack
+    
+    def flip_bit(num):
+        if num == 0:
+            return 1
+        return 0
+    def connect_gaia(ip, port, id):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, int(port)))
+        print("HELLO R {} {} {} {}".format(loss_rate, corrupt_rate, max_delay, id))
+        s.sendall(("HELLO R {} {} {} {}".format(loss_rate, corrupt_rate, max_delay, id)).encode())
+        while(True):
+            recieved_gaia = s.recv(1024)
+            recieved_gaia = recieved_gaia.decode()
+            if recieved_gaia.count("WAITING") > 0:
+                print(recieved_gaia)
+                continue
+            else:
+                break
+        if recieved_gaia.count("OK") > 0:
+            print("{} {}".format(recieved_gaia, datetime.datetime.now()))
+            return s
+        elif recieved_gaia.count("ERROR") > 0:
+            print("Cannot connect to gaia.cs.umass.edu, error message: " + recieved_gaia)
+        else:
+            print(recieved_gaia)
+
+    cur_seq_num = 0
+    file_recieved = ""
+    num_recieved = 0
+    c_sock = connect_gaia(server_ip, server_port, connection_ID)
+    while (True):
+        try:
+            recieved = c_sock.recv(1024)
+            recieved = recieved.decode()
+        except:
+            break
+        if recieved == "":
+            break
+        recv_seq_num = int(recieved[0])
+        data = recieved[4:24]
+        print("Recieved a packet")
+        num_recieved += 1
+        if not checksum_verifier(recieved) or recv_seq_num != cur_seq_num:
+            print("Packet is corrupt or not correct sequence num")
+            a = make_ack(flip_bit(cur_seq_num))
+            try:
+                c_sock.sendall(a.encode())
+            except:
+                break
+        else:
+            
+            if recv_seq_num == cur_seq_num:
+                try:
+                    c_sock.sendall(make_ack(cur_seq_num).encode())
+                except:
+                    break
+                cur_seq_num = flip_bit(cur_seq_num)
+                file_recieved += data
+
+    print(num_recieved)
+    checksum_val = checksum(file_recieved)
+        
+
+
+    print("Finish running receiver: {}".format(datetime.datetime.now()))
+
+    # PRINT STATISTICS
+    # PLEASE DON'T ADD ANY ADDITIONAL PRINT() AFTER THIS LINE
+    print("File checksum: {}".format(checksum_val))
+
+    return checksum_val
+
+ 
+if __name__ == '__main__':
+    # CHECK INPUT ARGUMENTS
+    if len(sys.argv) != 7:
+        print("Expected \"python PA2_receiver.py <server_ip> <server_port> <connection_id> <loss_rate> <corrupt_rate> <max_delay>\"")
+        exit()
+    server_ip, server_port, connection_ID, loss_rate, corrupt_rate, max_delay = sys.argv[1:]
+    # START RECEIVER
+    start_receiver(server_ip, int(server_port), connection_ID, loss_rate, corrupt_rate, max_delay)
---
+++ PA2_sender.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+# Last updated: Oct, 2021
+
+import sys
+import socket
+import datetime
+from checksum import checksum, checksum_verifier
+import select
+
+CONNECTION_TIMEOUT = 60 # timeout when the sender cannot find the receiver within 60 seconds
+FIRST_NAME = "FIRSTNAME"
+LAST_NAME = "LASTNAME"
+
+def start_sender(server_ip, server_port, connection_ID, loss_rate=0, corrupt_rate=0, max_delay=0, transmission_timeout=60, filename="declaration.txt"):
+    """
+     This function runs the sender, connnect to the server, and send a file to the receiver.
+     The function will print the checksum, number of packet sent/recv/corrupt recv/timeout at the end. 
+     The checksum is expected to be the same as the checksum that the receiver prints at the end.
+
+     Input: 
+        server_ip - IP of the server (String)
+        server_port - Port to connect on the server (int)
+        connection_ID - your sender and receiver should specify the same connection ID (String)
+        loss_rate - the probabilities that a message will be lost (float - default is 0, the value should be between 0 to 1)
+        corrupt_rate - the probabilities that a message will be corrupted (float - default is 0, the value should be between 0 to 1)
+        max_delay - maximum delay for your packet at the server (int - default is 0, the value should be between 0 to 5)
+        tranmission_timeout - waiting time until the sender resends the packet again (int - default is 60 seconds and cannot be 0)
+        filename - the path + filename to send (String)
+
+     Output: 
+        checksum_val - the checksum value of the file sent (String that always has 5 digits)
+        total_packet_sent - the total number of packet sent (int)
+        total_packet_recv - the total number of packet received, including corrupted (int)
+        total_corrupted_pkt_recv - the total number of corrupted packet receieved (int)
+        total_timeout - the total number of timeout (int)
+
+    """
+
+    print("Student name: {} {}".format("Nicholas", "Ankiewicz"))
+    print("Start running sender: {}".format(datetime.datetime.now()))
+
+    checksum_val = "00000"
+    total_packet_sent = 0
+    total_packet_recv = 0
+    total_corrupted_pkt_recv = 0
+    total_timeout =  0
+    store = ""
+    def make_packet(ack_num, seq_num, data):
+        if ack_num != 1 and ack_num != 0:
+            print("Ack # is not 1 or 0")
+            return None
+        if ack_num != 1 and ack_num != 0:
+            print("Squence # is not 1 or 0")
+            return None
+        ack_num = str(ack_num)
+        seq_num = str(seq_num)
+        pcket = "{} {} {} ".format(seq_num, ack_num, data)
+        if len(pcket) != 25:
+            print("Error making packet")
+            return None
+        pcket_checksum = checksum(pcket)
+        pcket = pcket + pcket_checksum
+        if (not checksum_verifier(pcket)):
+            print("Error making checksum")
+            return None
+        return pcket
+    def connect_gaia(ip, port, id):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, int(port)))
+        print("HELLO S {} {} {} {}".format(loss_rate, corrupt_rate, max_delay, id))
+        s.sendall(("HELLO S {} {} {} {}".format(loss_rate, corrupt_rate, max_delay, id)).encode())
+        while(True):
+            recieved_gaia = s.recv(1024)
+            recieved_gaia = recieved_gaia.decode()
+            if recieved_gaia.count("WAITING") > 0:
+                print(recieved_gaia)
+                continue
+            else:
+                break
+        if recieved_gaia.count("OK") > 0:
+            print("{} {}".format(recieved_gaia, datetime.datetime.now()))
+            return s
+        elif recieved_gaia.count("ERROR") > 0:
+            print("Cannot connect to gaia.cs.umass.edu, error message: " + recieved_gaia)
+        else:
+            print(recieved_gaia)
+
+    def flip_bit(num):
+        if num == 0:
+            return 1
+        return 0
+
+    print("Connecting to server: {}, {}, {}".format(server_ip, server_port, connection_ID))
+    s = connect_gaia(server_ip, server_port, connection_ID)
+    file = open(filename, 'r')
+    ack_num = 0
+    seq_num = 0
+    recieved_correct_ack_flag = -1
+    while (True):
+        data = file.read(20)
+        if data == '':
+            print("SENT ALL DATA!!!")
+            break
+        while (len(data) < 20):
+            data += " "
+        store += data
+        pcket = make_packet(ack_num, seq_num, data)
+        if ack_num == None or seq_num == None:
+            break
+        s.sendall(pcket.encode())
+        total_packet_sent += 1
+        print("Sent packet!")
+        recieved_correct_ack_flag = -1
+        while(recieved_correct_ack_flag != 1):
+            read_sock, _, _ = select.select([s], [], [], transmission_timeout)
+            if read_sock:
+                for s in read_sock:
+                    recieved = s.recv(1024)
+                    recieved = recieved.decode()
+                    total_packet_recv += 1
+                    print("Recieved: {}".format(recieved))
+                    if (checksum_verifier(recieved)):
+                        recieved_ack = recieved.split(" ")
+                        recieved_ack = list(filter(None, recieved_ack))
+                        if int(recieved_ack[0]) == seq_num:
+                            print("Recieved correct ack")
+                            recieved_correct_ack_flag = 1
+                            seq_num = flip_bit(seq_num)
+                            break
+                        else:
+                            print("Recieved incorrect ack")
+                    else:
+                        total_corrupted_pkt_recv += 1
+            else:
+                print("Timeout!: resent")
+                total_packet_sent += 1
+                total_timeout += 1
+                s.sendall(pcket.encode())
+    file.close()
+    print(len(store))
+    checksum_val = checksum(store)
+    ##### END YOUR IMPLEMENTATION HERE #####
+
+    print("Finish running sender: {}".format(datetime.datetime.now()))
+
+    # PRINT STATISTICS
+    # PLEASE DON'T ADD ANY ADDITIONAL PRINT() AFTER THIS LINE
+    print("File checksum: {}".format(checksum_val))
+    print("Total packet sent: {}".format(total_packet_sent))
+    print("Total packet recv: {}".format(total_packet_recv))
+    print("Total corrupted packet recv: {}".format(total_corrupted_pkt_recv))
+    print("Total timeout: {}".format(total_timeout))
+
+    return (checksum_val, total_packet_sent, total_packet_recv, total_corrupted_pkt_recv, total_timeout)
+ 
+if __name__ == '__main__':
+    # CHECK INPUT ARGUMENTS
+    if len(sys.argv) != 9:
+        print("Expected \"python3 PA2_sender.py <server_ip> <server_port> <connection_id> <loss_rate> <corrupt_rate> <max_delay> <transmission_timeout> <filename>\"")
+        exit()
+
+    # ASSIGN ARGUMENTS TO VARIABLES
+    server_ip, server_port, connection_ID, loss_rate, corrupt_rate, max_delay, transmission_timeout, filename = sys.argv[1:]
+    
+    # RUN SENDER
+    start_sender(server_ip, int(server_port), connection_ID, loss_rate, corrupt_rate, max_delay, float(transmission_timeout), filename)
